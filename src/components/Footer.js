29b01221ba4f572c6981061c1ab95c4c443351a6
Deanna Norman <deannaleenorman@msn.com>
2021-03-18 19:50:29 -0700

    updates footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,13 @@ function Footer () {
 
     return(
         <footer className="mt-5">
-            <Container>
-                <Row>
+            <Container fluid={true}>
+                <Row className="border-top justify-content-between p-3">
                     <Col className="p-0" md={3} sm={12}>
                         Deanna Norman
+                    </Col>
+                    <Col className="p-0 d-flex justify-content-end" md={3}>
+                        This site was made by Deanna Norman
                     </Col>
                 </Row>
             </Container>
